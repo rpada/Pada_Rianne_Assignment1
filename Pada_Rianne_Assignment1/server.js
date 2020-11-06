7909db43a4e9dd0aacc440400c5e4d45eee1372c
rpada <70246093+rpada@users.noreply.github.com>
2020-11-05 18:29:16 -1000

    assignment1

---
+++ Pada_Rianne_Assignment1/server.js
@@ -0,0 +1,148 @@
+// REFERENCE: FROM DANIEL PORT'S SCREENCAST "ASSIGNMENT 1"
+var express = require ('express');
+var app = express();
+var myParser = require("body-parser");
+var fs = require('fs');
+var data = require('./public/products.js');
+var products = data.products; 
+var qs = require("querystring");
+
+
+app.all('*', function (request, response, next) {
+console.log(request.method +'to'+ request.path);
+next();
+}); // checking to see if receiving correct results
+app.use(myParser.urlencoded({extended: true}));
+app.post("/process_order", function (request,response){
+// process_quantity_form(request.body, response);
+// response.send(request.body);
+// check if the quantities are valid or nonnegint first
+global.onload = function () {
+   let params = (new URL(document.location)).searchParams; // get the query string which has the form data
+   // form was submitted so check that quantities are valid then redirect to invoice if ok.
+   if (request.body('purchase_submit')) {
+       has_errors = false; // assume quantities are valid from the start
+       total_qty = 0; // need to check if something was selected so we will look if the total > 0
+       for (i = 0; i < products.length; i++) {
+         console.log(params.get(`quantity${i}`));
+           if (request.body(`quantity${i}`)) {
+               a_qty = params.get(`quantity${i}`);
+               // make textboxes sticky in case of invalid data
+               product_selection_form[`quantity${i}`].value = a_qty;
+               total_qty += a_qty; 
+               if(!isNonNegInt(a_qty)) {
+                   has_errors = false; // oops, invalid quantity
+                   checkQuantityTextbox(product_selection_form[`quantity${i}`]); // show where the error is
+               }
+           }
+       }
+has_errors = true;
+theqstring = qs.stringify(request.body);
+if (!has_errors) {
+   // if quantities are valid, generate the invoice from quantities
+ 
+ response.redirect("./invoice.html?" + theqstring);
+} else {
+ // got a mistake, just leave the user on the page dont respond 
+ response.redirect("./index.html?" + theqstring);
+}
+
+function isNonNegInt(q, return_errors = false) {
+   errors = []; // assume no errors at first
+   if(q == '') q =0; // handle blank inputs as if they are 0
+   if (Number(q) != q) errors.push('<font color="red">Not a number!</font>'); // Check if string is a number value
+   else if (q < 0) errors.push('<font color="red">Negative value!</font>'); // Check if it is non-negative
+   else if (parseInt(q) != q) errors.push('<font color="red">Not an integer!</font>'); // Check that it is an integer
+   return return_errors ? errors : (errors.length == 0);
+}
+
+       // Now respond to errors or redirect to invoice if all is ok
+       if(has_errors) {
+           alert("Please enter only valid quantities!");
+       } else if(total_qty == 0) { // no quantity selections, just give a general alert
+           alert("Please select some quantities!");
+       }
+   }
+}
+app.use(myParser.urlencoded({ extended: true }));
+app.post("/process_form", function (request, response) {
+   let POST = request.body;
+   if(isNonNegInt(POST["quantity_textbox"])) {
+       response.send(`Thank you for ordering ${POST ["quantity_textbox"]} items!`);
+   } else {
+        response.send(`Hey ${POST["quantity_textbox]"]}} is not valid!`); 
+   }
+});
+
+has_errors = true;
+theqstring = qs.stringify(request.body);
+if (!has_errors) {
+   // if quantities are valid, generate the invoice from quantities
+ 
+ response.redirect("./index.html?" + theqstring);
+} else {
+ // got a mistake, just leave the user on the page dont respond 
+ response.redirect("./invoice.html?" + theqstring);
+}
+
+// if non valid, send user back to page and show them errors to correct
+
+
+
+
+
+
+
+
+
+
+});
+app.use(express.static('./public'));
+app.listen(8080, () => console.log('listening on Port 8080'));
+
+function iNonNegInt (q,returnErrors = false) {
+    errors = []; // assume no error at first
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is number
+    if (q < 0) errors.push ('Negative value!'); // check if non-negative
+    if (parseInt (q) != q) errors.push('Not an interger!'); // check if interger
+    return returnErrors ? errors : (errors.length == 0);
+
+app.use(myParser.urlencoded({ extended: true }));
+//intercept purchase submission form, if good give an invoice, otherwise send back to order page
+app.get("/invoice", function (request, response) {
+   //check if quantity data is valid
+   //look up request.query
+   params = request.query;
+   console.log(params);
+   if (typeof params['purchase_submit'] != 'undefined') {
+      has_errors = false; // assume quantities are valid from the start
+      total_qty = 0; // need to check if something was selected so we will look if the total > 0
+      for (i = 0; i < products.length; i++) {
+         if (typeof params[`quantity${i}`] != 'undefined') {
+            a_qty = params[`quantity${i}`];
+            total_qty += a_qty;
+            if (!isNonNegInt(a_qty)) {
+               has_errors = true; // oops, invalid quantity
+            }
+         }
+      }
+      qstr = querystring.stringify(request.query);
+      // Now respond to errors or redirect to invoice if all is ok
+      if (has_errors || total_qty == 0) {
+         //if quantity data is not valid, send them back to product display
+         qstr = querystring.stringify(request.query);
+         response.redirect("index.html?" + qstr);
+      } else { // all good to go!
+         response.redirect("invoice.html?" + qstr);
+      }
+   }
+});
+
+app.use(express.static('./public'));
+app.listen(8080, () => console.log(`listening on port 8080`));
+response.send(receipt);
+response.end();
+}
+
+
+
---
+++ productsstore copy/public/products.js
@@ -0,0 +1,55 @@
+var products = [ //products in my shop
+    {
+    "name": "Candy Corn",
+    "price": 4.00,
+    "image": "https://i.ibb.co/5BRpwR9/55-D2-E449-9413-4-DC5-B559-05-D17513-CFAA.jpg" //image hosted on IMGBB
+    },
+    {
+    "name": "Pumpkin",
+    "price": 5.00,
+    "image": "https://i.ibb.co/r7DyS1H/762-A40-C6-ABF0-400-F-B16-A-1-B63-EAF7-AB0-A.jpg" //image hosted on IMGBB
+    },
+    {
+    "name": "Ghosts",
+    "price": 6.00,
+    "image": "https://i.ibb.co/2SNHJwt/DEA11867-FD58-4-A3-D-9-A7-B-386669-BB9986.jpg" //image hosted on IMGBB
+    },
+    {
+    "name": "Black Parade",
+    "price": 6.00,
+    "image": "https://i.ibb.co/271mrMs/33-BA8-C2-C-4-C40-4-D87-B980-E72-E786-D1-D6-C.jpg" //image hosted on IMGBB
+    },
+    {
+    "name": "Bold and Brash",
+    "price": 5.00,
+    "image": "https://i.ibb.co/0c19k1t/481066-CF-D5-B2-4391-9-D66-CF5-CF012-F16-A.jpg" //image hosted on IMGBB
+    },
+    {
+      "name": "Milk",
+      "price": 4.00,
+      "image": "https://i.ibb.co/0rnBmZk/329617-BE-AE86-4-FE3-B56-D-8-FA580-AD726-E.jpg" //image hosted on IMGBB
+      },
+      {
+    "name": "Sun and Moon Tarot",
+    "price": 4.00,
+    "image": "https://i.ibb.co/zR7k3P2/8374657-E-9-C71-450-F-9404-E059-FEDDAD96.jpg" //image hosted on IMGBB
+    },
+    {
+    "name": "Appa",
+    "price": 4.00,
+    "image": "https://i.ibb.co/XkScG0r/379-F6306-2-EA1-4-A74-9035-BB3-A83-E15-E41.jpg" //image hosted on IMGBB
+    },
+    {
+    "name": "Isabelle (Animal Crossing)",
+    "price": 4.00,
+    "image": "https://i.ibb.co/Z6BPR9S/CE318514-70-C7-4-B87-8-EA3-E1-AA4-BFCE51-B.jpg" //image hosted on IMGBB
+    }, 
+    {
+    "name": "Tom Nook (Animal Crossing)",
+    "price": 5.00,
+    "image": "https://i.ibb.co/YtVs6yR/2989-FDBC-61-E0-4959-9-C14-F578830-A331-E.jpg" //image hosted on IMGBB
+    }
+];
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
